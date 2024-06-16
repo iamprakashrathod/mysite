943ce15afe5e4cc0cf61a8dcd9fde18612fceb35
Prakash <prakashsrathod@gmail.com>
2024-06-16 17:22:34 +0530

    second commit

--- main/views.py
+++ main/views.py
@@ -1,16 +1,52 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
-from .models import Tutorial 
+from .models import Tutorial
 from django.contrib import messages
+from .forms import NewUserForm
+from .models import Tutorial, TutorialCategory, TutorialSeries
+
+
+
+def single_slug(request, single_slug):
+    # first check to see if the url is in categories.
+
+    categories = [c.category_slug for c in TutorialCategory.objects.all()]
+    if single_slug in categories:
+        matching_series = TutorialSeries.objects.filter(tutorial_category__category_slug=single_slug)
+        series_urls = {}
+
+        for m in matching_series.all():
+            part_one = Tutorial.objects.filter(tutorial_series__tutorial_series=m.tutorial_series).earliest("tutorial_published")
+            series_urls[m] = part_one.tutorial_slug
+
+        return render(request=request,
+                      template_name='main/category.html',
+                      context={"tutorial_series": matching_series, "part_ones": series_urls})
+
+
+    tutorials = [t.tutorial_slug for t in Tutorial.objects.all()]
+
+    if single_slug in tutorials:
+        this_tutorial = Tutorial.objects.get(tutorial_slug=single_slug)
+        tutorials_from_series = Tutorial.objects.filter(tutorial_series__tutorial_series=this_tutorial.tutorial_series).order_by('tutorial_published')
+        this_tutorial_idx = list(tutorials_from_series).index(this_tutorial)
+
+        return render(request=request,
+                      template_name='main/tutorial.html',
+                      context={"tutorial":this_tutorial,
+                               "sidebar":tutorials_from_series,
+                               "this_tut_idx":this_tutorial_idx})
 
 def homepage(request):
-    tutorials = Tutorial.objects.all()  # Assuming Tutorial is your model
-    return render(request, "main/home.html", {"tutorials": tutorials})
+    return render(request=request,
+                  template_name='main/categories.html',
+                  context={"categories": TutorialCategory.objects.all})
+
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = NewUserForm(request.POST)
         if form.is_valid():
             user = form.save()
             username = form.cleaned_data.get('username')
@@ -22,9 +58,37 @@ def register(request):
             for field, errors in form.errors.items():
                 for error in errors:
                     messages.error(request, f"{field}: {error}")
-            # You can also pass form.errors to the template to display errors
 
     else:
-        form = UserCreationForm()
+        form = NewUserForm()
 
     return render(request, "main/register.html", {"form": form})
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("main:homepage")
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect("main:homepage")
+            else:
+                messages.error(request, "Invalid username or password")
+        else:
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f"{field}: {error}")
+
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "main/login.html", {"form": form})
+   
